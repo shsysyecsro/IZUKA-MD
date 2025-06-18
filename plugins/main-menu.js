783d5624d5b99d76ae731875ebe9fc7582d01f82
Zarya-MD <dawenstech@gmail.com>
2025-06-18 10:07:36 -0400

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -64,11 +64,11 @@ async (conn, mek, m, { from, reply }) => {
 
     const keys = Object.keys(category).sort();
     for (let k of keys) {
-      menuText += `\n\n🌺╭─『 *${k.toUpperCase()}* 』\n`;
+      menuText += `\n\n🏳️‍🌈『 *${k.toUpperCase()}* 』\n`;
       const cmds = category[k].filter(c => c.pattern).sort((a, b) => a.pattern.localeCompare(b.pattern));
       cmds.forEach((cmd) => {
         const usage = cmd.pattern.split('|')[0];
-        menuText += `🌸 *${config.PREFIX}${toSmallCaps(usage)}*\n`;
+        menuText += `╭─🇭🇹 *${config.PREFIX}${toSmallCaps(usage)}*\n`;
       });
       menuText += `┕──────────────❒`;
     }

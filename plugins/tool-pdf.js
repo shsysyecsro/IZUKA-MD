6df033c54621bf58b5bc320d36387df4c73f53a4
Zarya-MD <dawenstech@gmail.com>
2025-06-19 20:36:46 -0400

    Update tool-pdf.js

--- plugins/tool-pdf.js
+++ plugins/tool-pdf.js
@@ -7,7 +7,7 @@ cmd({
     alias: ["pdf","topdf"],use: '.topdf',
     desc: "Convert provided text to a PDF file.",
     react: "📄",
-    category: "utilities",
+    category: "utility",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {

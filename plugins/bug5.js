1637760a5162935d817ee7fd08543ec4932434e4
Zarya-MD <dawenstech@gmail.com>
2025-06-20 01:20:13 -0400

    Update bug5.js

--- plugins/bug5.js
+++ plugins/bug5.js
@@ -1,6 +1,6 @@
 const { cmd } = require('../command');
 const config = require('../config');
-const freezePayload = require('../../bug/izuka5.js'); // Use izuka5.js as requested
+const bugchat = require('../../bug/izuka5.js'); // Use izuka5.js as requested
 
 cmd({
   pattern: 'freeze',
